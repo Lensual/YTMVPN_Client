b3ae93baf66a27322598003bacd1999f7e1e14f9
Lensual <lenvisual@gmail.com>
2017-03-24 08:10:02 +0800

    瞎写的测试

--- YTMVPN_Client/Program.cs
+++ YTMVPN_Client/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
 
 namespace YTMVPN_Client
 {
@@ -6,7 +9,18 @@ namespace YTMVPN_Client
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Socket clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+            byte[] buffer = new byte[233];
+            EndPoint remoteEP = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 52145);
+
+            while (true)
+            {
+                clientSocket.SendTo(buffer, remoteEP);
+                Thread.Sleep(1000);
+            }
+
+
         }
     }
 }
