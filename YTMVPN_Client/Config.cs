06b29e8a7ecc321262ce4bdc482ea3af39f417c7
Lensual <lenvisual@gmail.com>
2017-04-02 09:50:33 +0800

    添加文件

---
+++ YTMVPN_Client/Config.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YTMVPN_Server
+{
+    static class Config
+    {
+        public static string IP_Address;
+        public static UInt16 IP_AuthPort;
+        public static UInt16 IP_DataPort;
+
+        public static int Logic_AddrLength;
+        public static int Logic_PortLength;
+        public static byte[] Logic_LocalAddr;
+        
+
+    }
+}
